8e6cb3f8e1de3e74e6e3b33be73b72599e33ce8f
Tomas Salgado <tomassalgadodev@gmail.com>
2023-06-12 10:35:58 -0600

    feature: Build out header structure

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,9 +1,29 @@
 import React from "react";
 import './Header.css'
+import headerImage from '../assets/CrownLogoPlain.png';
+import facebookIcon from '../assets/facebook-black.png';
+import emailIcon from '../assets/email-black.png';
+import igIcon from '../assets/ig-black.png';
 
 const Header = () => {
     return (
-        <h1>This is the header</h1>
+        <div>
+            <div className="sub-heading-container">
+                <span>GOOD DRINK. GOOD FOOD. GOOD PEOPLE.</span>
+            </div>
+            <img className="header-img" src={headerImage} />
+            <div className="heading-buttons-container">
+                <div className="call-email-button-container">
+                    <button>CALL</button>
+                    <button>EMAIL</button>
+                </div>
+                <div className="social-button-container">
+                    <img src={facebookIcon} />
+                    <img src={emailIcon} />
+                    <img src={igIcon} />
+                </div>
+            </div>
+        </div>
     )
 }
 
