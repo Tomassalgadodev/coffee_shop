cd0bd1fc394775fd76c6bb2a30b5a30e4f5b04b7
Tomas Salgado <tomassalgadodev@gmail.com>
2023-06-12 08:44:17 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
