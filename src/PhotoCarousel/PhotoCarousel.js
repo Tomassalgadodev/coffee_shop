19685d384eb59a47dedbe1af46556de6757c1f8f
Tomas Salgado <tomassalgadodev@gmail.com>
2023-06-14 10:55:43 -0600

    Structure, style and add functionality to photo carousel

--- src/PhotoCarousel/PhotoCarousel.js
+++ src/PhotoCarousel/PhotoCarousel.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import './PhotoCarousel.css';
 
+import carouselImage1 from '../assets/carousel-image1.jpeg';
+import carouselImage2 from '../assets/carousel-image2.jpeg';
+import rightArrow from '../assets/right-arrow.png';
+import leftArrow from '../assets/left-arrow.png';
+
 const PhotoCarousel = () => {
+    const [currentPhoto, setCurrentPhoto] = useState(carouselImage1);
+    const currentPhotos = [];
+
+    const changePhoto = () => {
+        currentPhoto === carouselImage1 ? setCurrentPhoto(carouselImage2) : setCurrentPhoto(carouselImage1);
+    }
+
     return (
-        <h1>photoCarousel</h1>
+        <div className="carousel-container">
+            <div className="carousel">
+                <div className="photo-container" style={{ backgroundImage: `url(${currentPhoto})`, backgroundSize: 'contain' }}>
+                    <div className="left-arrow"
+                        onClick={changePhoto}
+                    >
+                        <img className="carousel-arrow" src={leftArrow} />
+                    </div>
+                    <div className="right-arrow"
+                        onClick={changePhoto}
+                    >
+                        <img className="carousel-arrow" src={rightArrow} />
+                    </div>
+                </div>
+                <div className="selectors-container">
+                    <img style={{ opacity: currentPhoto === carouselImage1 ? '.5' : '1' }} className="selector-image" src={carouselImage1}/>
+                    <img style={{ opacity: currentPhoto === carouselImage2 ? '.5' : '1' }} className="selector-image" src={carouselImage2}/>
+                </div>
+            </div>
+        </div>
     )
 }
 
